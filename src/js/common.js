601aca27dac6956038ada676a074fccea4190597
Alekss <x0rtex420@gmail.com>
2025-04-08 18:49:54 +0100

    Website is now built with Webpack to allow for node modules to be used

---
+++ src/js/common.js
@@ -0,0 +1,66 @@
+import '../css/styles.css'
+
+document.addEventListener("DOMContentLoaded", () => {
+    createNavbar();
+    initializePlayer();
+});
+
+const websiteUrl = "https://musicstreamcc.xyz";
+export const apiEndpoint = "https://api.musicstreamcc.xyz/";
+export const placeholderImg = "https://placehold.co/600x600?text=No+Image";
+
+function createNavbar() {
+    const navContainer = document.getElementById('nav-container');
+    navContainer.innerHTML = `
+      <!-- Relative URLs are intentional and will be resolved correctly in the rendered HTML -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="index.html">MusicStream</a>
+          <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="profile.html">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="checkout.html">Checkout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      `;
+}
+
+function initializePlayer() {
+    if (!document.getElementById("music-player-container")) {
+        const playerContainer = document.createElement("div");
+        playerContainer.innerHTML = `
+      <div id="music-player-container" class="bg-black bg-opacity-25 p-2">
+        <audio id="music-player" class="w-100" controls>
+          <source src="" type="audio/mpeg">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    `;
+        document.body.appendChild(playerContainer);
+    }
+}
+
+export function getArtistImageUrl(artistId) {
+    return `${websiteUrl}/media/images/artists/${artistId}.webp`;
+}
+
+export function getAlbumImageUrl(albumId) {
+    return `${websiteUrl}/media/images/albums/${albumId}.webp`;
+}
+
+export function getSongImageUrl(songId) {
+    return `${websiteUrl}/media/images/songs/${songId}.webp`;
+}
+
+export function getSongAudioUrl(songId) {
+    return `${websiteUrl}/media/audio/songs/${songId}.mp3`;
+}
